9409e029da7377dee91e152c16e42cc3697cb106
Kalip276 <dsa151109@gmail.com>
2024-08-29 22:51:11 +0700

    Update Driverinstall.bat

--- Driverinstall.bat
+++ Driverinstall.bat
@@ -1,14 +1,14 @@
 @echo off
 title Downloading Nvidia driver...
-echo The drivers are 664Mb to 855Mb, so this will take a moment to download. (696,519,945 or 897,058,453 bytes)
+echo The drivers are 528 Mb to 717 Mb, so this will take a moment to download. (553,751,625 or 752,433,782 bytes)
 echo PLEASE, do NOT open HoneCtrl until the driver installation is done.
 echo.
 :start
 set /p choice=Do you need shadowplay and other components of the driver? Y or N?: 
 if /i "%choice%" == "y" (
-  curl -g -L -# -o "C:\Hone\Drivers\NvidiaHone.exe" "https://github.com/auraside/HoneCtrl/releases/download/2.58/HoneDefault.exe"
+  curl -g -L -# -o "C:\Hone\Drivers\NvidiaHone.exe" "https://github.com/Kalip276/file/releases/download/gwgwg/546.17_Bloat.exe"
 ) else if /i "%choice%" == "n" (
-  curl -g -L -# -o "C:\Hone\Drivers\NvidiaHone.exe" "https://github.com/auraside/HoneCtrl/releases/download/2.58/HoneTweaked.exe"
+  curl -g -L -# -o "C:\Hone\Drivers\NvidiaHone.exe" "https://github.com/Kalip276/file/releases/download/gwgwg/546.17_Clean.exe"
 ) else (
   goto start
 )
